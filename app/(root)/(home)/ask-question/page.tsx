c4c917d034f6e9ea7eae7e253f9045fb80880420
YOOUMUU <yooumuu@gmail.com>
2024-01-16 23:08:03 +0800

    page - ask a question

--- app/(root)/(home)/ask-question/page.tsx
+++ app/(root)/(home)/ask-question/page.tsx
@@ -1,7 +1,19 @@
-import React from "react";
+import Question from '@/components/forms/Question';
+import type { Metadata } from 'next';
 
-const page = () => {
-  return <div>page</div>;
+export const metadata: Metadata = {
+  title: 'Ask a Question | DevOverFlow',
 };
 
-export default page;
+const Page = async () => {
+  return (
+    <div>
+      <h1 className="h1-bold text-dark100_light900">Ask a Question </h1>
+      <div className="mt-9">
+        <Question />
+      </div>
+    </div>
+  );
+};
+
+export default Page;
